7901519a8be091f5f3cb9532f94df9cf65e39e03
ShahabMorshedi <sh.morshedi@hotmail.com>
2022-01-14 01:00:03 +0330

    Prefix Code

---
+++ CodinGame/PREFIXCODE/PrefixCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinGame.PREFIXCODE
+{
+    public class PrefixCode
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                string b = inputs[0];
+                int c = int.Parse(inputs[1]);
+            }
+            string s = Console.ReadLine();
+
+            // Write an answer using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("abracadabra");
+        }
+    }
+}
